d61cf305f4206c81af4743e87bcf45fcbd858005
Haldo Somoza <haldo_somoza@yahoo.com>
2024-07-31 19:57:16 -0400

    Removing test cede.

--- app.py
+++ app.py
@@ -56,19 +56,6 @@ def api_prediction_post():
     resultThisMsg = _evaluate_message(userFrom, message)
     # evaluating all messages from the user to the other user
     resultAllMsgs = _evaluate_all_messages(userFrom, userTo, message)
-
-
-    import psutil
-    # Get the memory details
-    memory_info = psutil.virtual_memory()
-    # Display the memory usage
-    print(f"===============================================")
-    print(f"Total Memory: {memory_info.total / (1024 ** 3):.2f} GB")
-    print(f"Available Memory: {memory_info.available / (1024 ** 3):.2f} GB")
-    print(f"Used Memory: {memory_info.used / (1024 ** 3):.2f} GB")
-    print(f"Memory Percentage: {memory_info.percent}%")
-    print(f"===============================================")
-
     
     # returning the results
     return { 
